713c37d44d50e6a32cfff166d34e8bcbe2479b21
hassan <hassanmorad5150@gmail.com>
2023-11-02 21:57:57 +0200

    kkjk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';  // Updated import
-import { createHashHistory } from 'history';
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom'; 
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
